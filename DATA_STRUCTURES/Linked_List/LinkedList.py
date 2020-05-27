3b01a5d242fd1d2e8e08839f6aabcafe23ed14c7
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-27 19:21:25 +0530

    made the function for adding the node to the beginning

--- DATA_STRUCTURES/Linked_List/LinkedList.py
+++ DATA_STRUCTURES/Linked_List/LinkedList.py
@@ -4,3 +4,10 @@ from DATA_STRUCTURES.Linked_List.Node import Node
 class LinkedList:
     def __int__(self):
         self.start=None
+    def insert_Node_atTheBeginning(self,data):
+        "this function will insert the node at the beginning"
+        newnode=Node(data)
+        newnode.next=self.start
+        #the next node of the new node is start node....so that new node can be added.
+        self.start=newnode
+
