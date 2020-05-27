3df906a28b4d83a3079bcb00fb2a12d1a0bb0ff5
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-27 19:27:28 +0530

    made the funtion for inserting at the given position

--- DATA_STRUCTURES/Linked_List/LinkedList.py
+++ DATA_STRUCTURES/Linked_List/LinkedList.py
@@ -10,4 +10,15 @@ class LinkedList:
         newnode.next=self.start
         #the next node of the new node is start node....so that new node can be added.
         self.start=newnode
+    def insert_atTheGivenNode(self,prev_Node,data):
+        "this function will help us to add the node at the particular postion"
+        if prev_Node is None:
+            print("this the beginning")
+            return
+        newNode=Node(data)
+        newNode.next=prev_Node.next
+        #Making the new node and making the previous node next as new node next
+        prev_Node.next=newNode
+        #Hence making the previous node next as newNode
+
 
