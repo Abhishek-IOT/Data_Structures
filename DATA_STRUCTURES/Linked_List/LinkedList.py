4d749ab8dab2df70b5b647725482aca35e5ac062
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-27 19:34:26 +0530

    made the method for inserting the node at the last

--- DATA_STRUCTURES/Linked_List/LinkedList.py
+++ DATA_STRUCTURES/Linked_List/LinkedList.py
@@ -4,12 +4,18 @@ from DATA_STRUCTURES.Linked_List.Node import Node
 class LinkedList:
     def __int__(self):
         self.start=None
+
+
     def insert_Node_atTheBeginning(self,data):
         "this function will insert the node at the beginning"
         newnode=Node(data)
         newnode.next=self.start
         #the next node of the new node is start node....so that new node can be added.
         self.start=newnode
+
+
+
+
     def insert_atTheGivenNode(self,prev_Node,data):
         "this function will help us to add the node at the particular postion"
         if prev_Node is None:
@@ -20,5 +26,19 @@ class LinkedList:
         #Making the new node and making the previous node next as new node next
         prev_Node.next=newNode
         #Hence making the previous node next as newNode
+
+    def insert_atTheEnd(self,data):
+        "This Function is for inserting at the end of the Linked List"
+        newNode=Node(data)
+        #Now checking that the Linked list os empty if it is empty then the NewNode will be the starting Node
+        if self.start is None:
+            self.start=newNode
+            return
+        #Now if this is not the case then we will have to traverse the whole list
+        last_Node=self.start
+        while(last_Node.next):
+            last_Node=last_Node.next
+        #After traversing the node we will update the last Node'next to the new Node
+        last_Node.next=newNode
 
 
