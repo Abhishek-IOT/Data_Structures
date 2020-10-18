d12d710d3e623bfd7d1efd4278111309cca82aa2
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-10-18 20:31:38 +0530

    class for rearrangement of arrays

---
+++ DATA_STRUCTURES/Array/Rearangment.py
@@ -0,0 +1,23 @@
+class Rearangment:
+    """
+    This is the method for rearrangment of the array
+    """
+    def fix(self,arr):
+        for i in range(0,len(arr)):
+            if arr[i]!=-1 and arr[i]!=i:
+                x=arr[i]
+                print(x,"The value of x")
+            while(arr[x]!=-1 and arr[x]!=x):
+                y=arr[x]
+                arr[x]=x
+                x=y
+                print(y,"Tje value of y")
+
+
+        for i in range(0,len(arr)):
+            print(arr[i],end=" ")
+
+if __name__ == '__main__':
+    rearrange=Rearangment()
+    arr=[3,1,2,0,4]
+    rearrange.fix(arr)
