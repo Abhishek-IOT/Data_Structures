ef940a6f021e3f234765c18680f183bb44cfacd8
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-12-23 18:16:50 +0530

    This is the question which this program is dealing with.

--- DATA_STRUCTURES/DSA Questions/KSmallest.py
+++ DATA_STRUCTURES/DSA Questions/KSmallest.py
@@ -1,3 +1,14 @@
+"""
+Given an array arr[] and a number K where K is smaller than size of array,
+ the task is to find the Kth smallest element in the given array.
+It is given that all array elements are distinct
+For Example=
+Input=
+Size of Array=6
+Array=7 10 4 3 20 15
+KTh position to find the minimum=3
+Output=7
+"""
 def mergesort(arr):
     if len(arr)>1:
         mid=len(arr)//2
