7ded49a331436885538ce093bb3fa1fea76bd7e0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-12-25 22:30:53 +0530

    rotate2 is more efficient.

--- DATA_STRUCTURES/DSA Questions/Cyclic_rotate.py
+++ DATA_STRUCTURES/DSA Questions/Cyclic_rotate.py
@@ -4,14 +4,19 @@ INPUT=1 2 3 4 5
 OUPUT=5 1 2 3 4
       OR
 OUPUT=2 3 4 5 1
-"""
-def rotate(arr):
+# """
+# def rotate(arr):
+#     temp=arr[0]
+#     for i in range(1,len(arr)):
+#         arr[i-1]=arr[i]
+#     arr[len(arr)-1]=temp
+#     print(arr)
+def rotate2(arr):
     temp=arr[0]
-    for i in range(1,len(arr)):
+    for i in range(len(arr),1):
         arr[i-1]=arr[i]
-    arr[len(arr)-1]=temp
     print(arr)
-
 if __name__ == '__main__':
     arr=[1,2,3,4,5]
     rotate(arr)
+    rotate2(arr)
