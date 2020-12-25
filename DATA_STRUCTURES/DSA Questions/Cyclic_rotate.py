37b77f9c09b2bf7ab8e8483752aa39da5d0928e2
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-12-25 22:38:04 +0530

    implement function for rotating it right direction

--- DATA_STRUCTURES/DSA Questions/Cyclic_rotate.py
+++ DATA_STRUCTURES/DSA Questions/Cyclic_rotate.py
@@ -16,7 +16,15 @@ def rotate2(arr):
     for i in range(len(arr),1):
         arr[i-1]=arr[i]
     print(arr)
+def implement(arr):
+    arr2=[]
+    print(arr[len(arr)-1])
+    arr2.append(arr[len(arr)-1])
+    for i in range(0,len(arr)-1):
+        arr2.append(arr[i])
+    print(arr2)
 if __name__ == '__main__':
     arr=[1,2,3,4,5]
-    rotate(arr)
+    # rotate(arr)
     rotate2(arr)
+    implement(arr)
