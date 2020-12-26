e9036cbc646ea26d58b915e9fa5080f37437fa99
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-12-26 09:09:05 +0530

    added the description of the problem.

--- DATA_STRUCTURES/DSA Questions/Kadane.py
+++ DATA_STRUCTURES/DSA Questions/Kadane.py
@@ -1,3 +1,9 @@
+"""
+Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+INPUT=[-2, -3, 4, -1, -2, 1, 5, -3]
+OUTPUT=7
+Solved using dynamic programming
+"""
 def maxsubstring(arr,size):
     max_so_far=0
     max_ending=0
