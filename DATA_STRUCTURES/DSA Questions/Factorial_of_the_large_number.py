80995284b93bdec5fe1a8fe010aeb81065fbeb18
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-02 16:13:50 +0530

    Expalnation of Factorial_of_the_large_number.py

--- DATA_STRUCTURES/DSA Questions/Factorial_of_the_large_number.py
+++ DATA_STRUCTURES/DSA Questions/Factorial_of_the_large_number.py
@@ -1,3 +1,15 @@
+"""
+Factorials of large numbers
+Given an integer, the task is to find factorial of the number.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.
+The first line of each test case is N,the number whose factorial is to be found
+
+Output:
+Print the factorial of the number in separate line
+"""
+
 def factorial(inp):
     if inp==1:
         return 1
