42da4551cab1e978e0395c26c33f5c700981b815
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-02 15:55:39 +0530

    Explanation and logic of the program of subarraywith0.py

--- DATA_STRUCTURES/DSA Questions/subarraywith0.py
+++ DATA_STRUCTURES/DSA Questions/subarraywith0.py
@@ -1,3 +1,21 @@
+"""
+Subarray with 0 sum
+Given an array of positive and negative numbers.
+Find if there is a subarray (of size at-least one) with 0 sum.
+Input:
+5
+4 2 -3 1 6
+
+Output:
+Yes
+
+Explanation:
+2, -3, 1 is the subarray
+with sum 0.
+HINT=We will use the data set called set for it
+Logic=If the sum is 0 then return 0 else store the sum in set so that if sum is in the set that
+means there is an sub array with sum=0
+"""
 def sum(arr, size):
     s=set()
     sumi = 0
