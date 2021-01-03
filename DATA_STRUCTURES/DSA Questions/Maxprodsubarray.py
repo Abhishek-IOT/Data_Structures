4a91d7ffc738a7e1d814e879b350be3fd3f126b8
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-03 15:24:35 +0530

    Explanation of Maxprodsubarray.py

--- DATA_STRUCTURES/DSA Questions/Maxprodsubarray.py
+++ DATA_STRUCTURES/DSA Questions/Maxprodsubarray.py
@@ -1,3 +1,12 @@
+"""
+Maximum Subarray Product
+Given an array that contains both positive and negative integers, find the product of the maximum product subarray. Expected Time complexity is O(n) and only O(1) extra space can be used.
+
+Examples:
+
+Input: arr[] = {6, -3, -10, 0, 2}
+Output:   180  // The subarray is {6, -3, -10}
+"""
 def maxm(arr,size):
     max_end=1
     min_end=1
