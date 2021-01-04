318fcf510e4caac8b817af37d249fc023a8994ee
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-04 10:31:20 +0530

    Done the code of the Tripletsum.py

---
+++ DATA_STRUCTURES/DSA Questions/Tripletsum.py
@@ -0,0 +1,37 @@
+def sort(arr,size):
+    for i in range(size):
+        for j in range(size):
+            if arr[i]<arr[j]:
+                temp=arr[i]
+                arr[i]=arr[j]
+                arr[j]=temp
+    print(arr)
+def triplet(arr,s,k):
+    sort(arr,s)
+    l=0
+    for i in range(s-2):
+        first_pointer=i+1
+        second_pointer=s-1
+        while(first_pointer<second_pointer):
+            l=arr[i]+arr[first_pointer]+arr[second_pointer]
+            if l == k:
+                return l
+            elif l>k:
+                second_pointer=second_pointer-1
+            else:
+                first_pointer=first_pointer+1
+    return l
+
+
+
+
+
+if __name__ == '__main__':
+    arr=[1, 4, 45, 6, 10, 8]
+    k=22
+    # sort(arr,len(arr))
+    l=triplet(arr,len(arr),k)
+    if l==k:
+        print("Yes")
+    else:
+        print("No")
---
+++ DATA_STRUCTURES/DSA Questions/issubset.py
@@ -0,0 +1,18 @@
+# def issubset(arr1,arr2,n1,n2):
+#     for i in range(n2):
+#         for j in range(n1):
+#             if (arr2[i]==arr1[j]):
+#                 break
+#             if j==n1:
+#                 return 0
+#     return 1
+# if __name__ == '__main__':
+#     arr1 = [11, 1, 13, 21, 3, 7]
+#     arr2 =
+#
+#     m = len(arr1)
+#     n = len(arr2)
+#     if (issubset(arr1,arr2,m,n)):
+#         print("YEes")
+#     else:
+#         print("NO")
