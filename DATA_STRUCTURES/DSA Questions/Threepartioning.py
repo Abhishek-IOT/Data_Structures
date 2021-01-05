f0b465926da03b00df9eb4982031de9718dba22e
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-05 16:54:27 +0530

    Explanation of the problem Threepartioning.py

--- DATA_STRUCTURES/DSA Questions/Threepartioning.py
+++ DATA_STRUCTURES/DSA Questions/Threepartioning.py
@@ -1,3 +1,18 @@
+"""
+Three way partitioning of an array around a given range
+Given an array and a range [lowVal, highVal], partition the array around the range such that array is divided in three parts.
+1) All elements smaller than lowVal come first.
+2) All elements in range lowVal to highVVal come next.
+3) All elements greater than highVVal appear in the end.
+The individual elements of three sets can appear in any order
+We can simply sort the array and get the ans(but it takes more time) but we will use this logic to make the ans in less time.
+We will have to pointer first start and then end and we will check if the arr[i] is greater or lesser than the range and
+then set the array accordingly
+if the value is less than low push it in start and increment the start
+if the value is more then high push at the end and decrement the end
+
+"""
+
 def threeway(arr,size,low,high):
     start=0
     end=size-1
