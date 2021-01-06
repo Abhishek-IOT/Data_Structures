3200d6d248328310cd63f25db608e6ee087e0baa
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-06 09:26:03 +0530

    Explanation of problem SpiralTraversal.py

--- DATA_STRUCTURES/DSA Questions/Matrix/SpiralTraversal.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/SpiralTraversal.py
@@ -1,3 +1,18 @@
+"""
+Spiral Printing of Matrix
+Print a given matrix in spiral form
+Difficulty Level : Medium
+ Last Updated : 01 Dec, 2020
+Given a 2D array, print it in spiral form. See the following examples.
+
+Examples:
+
+Input:  1    2   3   4
+        5    6   7   8
+        9   10  11  12
+        13  14  15  16
+Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+"""
 def Traverse(arr,end_row,col_end):
     k=0
     l=0
