2fdbd62a3b56f23ccd57ed56023e81eabf32b096
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-08 10:08:10 +0530

    Expalnation of Medianofsortedrow.py

--- DATA_STRUCTURES/DSA Questions/Matrix/Medianofsortedrow.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/Medianofsortedrow.py
@@ -1,3 +1,21 @@
+"""
+Given a row wise sorted matrix of
+ size RxC where R and C are always odd, find the median of the matrix
+ Input:
+R = 3, C = 3
+M = [[1, 3, 5],
+     [2, 6, 9],
+     [3, 6, 9]]
+
+Output: 5
+
+Explanation:
+Sorting matrix elements gives us
+{1,2,3,3,5,6,6,9,9}. Hence, 5 is median.
+Logic=Transform the matrix in array and sort it then take the median
+"""
+
+
 def median(arr,row,col):
     arr1=[]
     for i in range(row):
