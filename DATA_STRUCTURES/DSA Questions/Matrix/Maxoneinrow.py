053b1601c4c37d23d90fdb168617b22c798f0bee
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-09 23:29:00 +0530

    explanation of the problem Maxoneinrow.py

--- DATA_STRUCTURES/DSA Questions/Matrix/Maxoneinrow.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/Maxoneinrow.py
@@ -1,3 +1,18 @@
+"""
+Row with max 1s
+Medium Accuracy: 37.09% Submissions: 9529 Points: 4
+Given a boolean 2D array of n x m dimensions where each row is sorted.
+Find the 0-based index of the first row that has the maximum number of 1's.
+Input:
+N = 4 , M = 4
+Arr[][] = {{0, 1, 1, 1},
+           {0, 0, 1, 1},
+           {1, 1, 1, 1},
+           {0, 0, 0, 0}}
+Output: 2
+Explanation: Row 2 contains 4 1's (0-based
+indexing).
+"""
 def maxrow(arr,row,col):
     s = []
     for i in range(row):
