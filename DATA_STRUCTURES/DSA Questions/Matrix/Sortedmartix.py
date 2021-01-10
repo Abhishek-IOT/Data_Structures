8ab4e816b70e2b3987d707bfb0541dc7062cadb9
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-10 18:30:53 +0530

    Explanation of the program Sortedmartix.py

--- DATA_STRUCTURES/DSA Questions/Matrix/Sortedmartix.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/Sortedmartix.py
@@ -1,3 +1,14 @@
+"""
+Sorted matrix
+Basic Accuracy: 52.25% Submissions: 11716 Points: 1
+Given an n x n matrix, where every row and column is sorted in non-decreasing order. Print all elements of matrix in sorted order.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an integer n denoting the size of the matrix.
+ Then the next line contains the n x n elements in row wise order.
+ Logic=First convert the matrix to a normal list and then sort and return that list
+"""
+
 def sort(arr,size):
     temp=0
     for i in range(size):
