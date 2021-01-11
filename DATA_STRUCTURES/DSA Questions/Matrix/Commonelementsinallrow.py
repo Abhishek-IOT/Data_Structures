7a8b54b0f415ee08460ed38e848cb843cb7ac53a
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-11 08:12:50 +0530

    Explanation of Commonelementsinallrow.py and logic

--- DATA_STRUCTURES/DSA Questions/Matrix/Commonelementsinallrow.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/Commonelementsinallrow.py
@@ -1,3 +1,24 @@
+"""
+Common elements in all rows of a given matrix
+Difficulty Level : Medium
+ Last Updated : 18 Feb, 2020
+Given an m x n matrix, find all common elements present in all rows in O(mn) time and one traversal of matrix.
+
+Example:
+
+Input:
+mat[4][5] = {{1, 2, 1, 4, 8},
+             {3, 7, 8, 5, 1},
+             {8, 7, 7, 3, 1},
+             {8, 1, 2, 7, 9},
+            };
+
+Output:
+1 8 or 8 1
+Logic=TO create a map of the first row and then check in each row if the element of the first row is present or not and
+if the element is present increment the count of elements and print at the last.
+"""
+
 def common(mat,row,col):
     mp=dict()
     for j in range(col):
