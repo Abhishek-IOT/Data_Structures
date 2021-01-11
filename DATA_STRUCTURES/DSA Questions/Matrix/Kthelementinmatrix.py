18aca146789e53fc83b2c5c2b0f973168a53f63a
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-11 07:57:04 +0530

    Explanation of the Kthelementinmatrix.py and logic

--- DATA_STRUCTURES/DSA Questions/Matrix/Kthelementinmatrix.py
+++ DATA_STRUCTURES/DSA Questions/Matrix/Kthelementinmatrix.py
@@ -1,3 +1,20 @@
+"""
+Kth element in Matrix
+Medium Accuracy: 47.64% Submissions: 9554 Points: 4
+Given a N x N matrix, where every row and column is sorted in non-decreasing order. Find the kth smallest element in the matrix.
+
+Example 1:
+Input:
+N = 4
+mat[][] =     {{16, 28, 60, 64},
+                   {22, 41, 63, 91},
+                   {27, 50, 87, 93},
+                   {36, 78, 87, 94 }}
+K = 3
+Output: 27
+Explanation: 27 is the 3rd smallest element.
+Logic=First convert the matrix to array and then sort the array then print the kth element.
+"""
 def convert(mat,row,col,k):
     arr=[]
     for i in range(row):
@@ -7,10 +24,10 @@ def convert(mat,row,col,k):
     print(arr)
     print(arr[k])
 if __name__ == '__main__':
-    mat=[[16, 28, 60, 64],
-                   [22, 41, 63, 91],
-                   [27, 50, 87, 93],
-                   [36, 78, 87, 94 ]]
-    k=3
+    mat=[[10, 20, 30, 40],
+                   [15, 25, 35, 45],
+                   [24, 29, 37, 48],
+                   [32, 33, 39, 50]]
+    k=7
     convert(mat,4,4,k-1)
 
