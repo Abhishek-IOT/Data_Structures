9bbe834f079795daf39ebebdfedc2b552ef711de
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-11 08:32:35 +0530

    Explanation the program reverse.py

--- DATA_STRUCTURES/DSA Questions/Strings/reverse.py
+++ DATA_STRUCTURES/DSA Questions/Strings/reverse.py
@@ -1,3 +1,20 @@
+"""
+344. Reverse String
+Easy
+
+1938
+
+744
+
+Add to List
+
+Share
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+"""
 def reverse(arr):
     l=len(arr)
     i=l
