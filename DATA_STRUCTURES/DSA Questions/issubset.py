c6ed59b7c5a871d450c59f776c0baf22863f66f1
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-20 16:04:03 +0530

    Done the program of sum is subset

--- DATA_STRUCTURES/DSA Questions/issubset.py
+++ DATA_STRUCTURES/DSA Questions/issubset.py
@@ -1,18 +1,41 @@
-# def issubset(arr1,arr2,n1,n2):
-#     for i in range(n2):
-#         for j in range(n1):
-#             if (arr2[i]==arr1[j]):
-#                 break
-#             if j==n1:
-#                 return 0
-#     return 1
-# if __name__ == '__main__':
-#     arr1 = [11, 1, 13, 21, 3, 7]
-#     arr2 =
-#
-#     m = len(arr1)
-#     n = len(arr2)
-#     if (issubset(arr1,arr2,m,n)):
-#         print("YEes")
-#     else:
-#         print("NO")
+"""
+Subset Sum Problem | DP-25
+Difficulty Level : Medium
+ Last Updated : 09 Nov, 2020
+
+Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
+
+Example:
+
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True
+There is a subset (4, 5) with sum 9.
+Logic =We are using the D.P Top down approach for the problem.(It is 0/1 knapsack problem
+
+"""
+def sumsubset(arr,cap,n):
+    dp = ([[False for i in range(sum + 1)]
+           for i in range(n + 1)])
+    for i in range(n+1):
+        for j in range(cap+1):
+            if i==0:
+                dp[i][j]=False
+            if j==0:
+                dp[i][j]=True
+    for i in range(n+1):
+        for j in range(cap+1):
+            if arr[i-1]<=j:
+                dp[i][j]=dp[i][j-arr[i-1]] or dp[i-1][j]
+            else:
+                dp[i][j] =dp[i-1][j]
+    return dp[n][cap]
+if __name__ == '__main__':
+    arr=[3, 34, 4, 12, 5, 2]
+    sum=9
+    n=len(arr)+1
+    net=sum+1
+    y=sumsubset(arr,sum,len(arr))
+    if y==True:
+        print("Yes found")
+    else:
+        print("No")
