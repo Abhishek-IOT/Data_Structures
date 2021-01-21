d7337afa7cbbb97b345ada10f9f447ddea43f3ac
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-21 17:53:45 +0530

    code and logic of Sticklerthief.py.

---
+++ DATA_STRUCTURES/DSA Questions/Searching and sorting/Sticklerthief.py
@@ -0,0 +1,31 @@
+"""
+Stickler Thief
+Easy Accuracy: 43.3% Submissions: 8860 Points: 2
+Stickler the thief wants to loot money from a society having n houses in a single line. He is a weird person and follows a certain rule
+when looting the houses. According to the rule, he will never loot two consecutive houses. At the same time, he wants to maximize the amount he loots.
+ The thief knows which house has what amount of money but is unable to come up with an optimal looting strategy.
+ He asks for your help to find the maximum money he can get if he strictly follows the rule. Each house has a[i] amount of money present in it.
+ Logic we will use  dynamic approach and check if the element should be included or not we will do this with dynamic programming.
+"""
+def maxvalue(arr):
+    l=len(arr)
+    if l==0:
+        return 0
+    if l==1:
+        return arr[1]
+    if l==2:
+        return max(arr[0],arr[1])
+    dp=[0]*l
+    dp[0]=arr[0]
+    dp[1]=max(arr[0],arr[1])
+    for i in range(2, l):
+        dp[i] = max(arr[i] + dp[i - 2], dp[i - 1])
+
+    return dp[-1]
+if __name__ == '__main__':
+    hval = [6, 7, 1, 3, 8, 2, 4]
+
+    # number of houses
+    n = len(hval)
+    print("Maximum loot value : {}".
+          format(maxvalue(hval)))
