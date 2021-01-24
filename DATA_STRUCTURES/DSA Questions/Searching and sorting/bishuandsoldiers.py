6634c92ca259f35c5fea1869885d27f9038cf07b
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-24 18:18:10 +0530

    Done some modifications in bishuandsoldiers.py

--- DATA_STRUCTURES/DSA Questions/Searching and sorting/bishuandsoldiers.py
+++ DATA_STRUCTURES/DSA Questions/Searching and sorting/bishuandsoldiers.py
@@ -1,4 +1,5 @@
 def binarysearch(arr,start,end,x):
+
     if end>=start:
         mid=start+(end-start)//2
         if arr[mid]==x:
@@ -10,14 +11,19 @@ def binarysearch(arr,start,end,x):
     else:
         return -1
 def bishu(arr,power):
-    index=binarysearch(arr,0,len(arr),power)
+    if power>arr[-1]:
+        index=arr[-1]
+    else:
+        index=binarysearch(arr,0,len(arr),power)
     s=0
     for i in range(index):
         s=s+arr[i]
+        if index==len(arr):
+            index=index-1
     return s,index+1
 if __name__ == '__main__':
-    arr=[1,2,3]
-    m=3
+    arr=[1 ,2 ,3 ,4 ,5 ,6 ,7]
+    m=10
     yo=bishu(arr,m)
     print(yo[0],"The sum")
     print(yo[1],"Count of the soldiers")
