55932a42403c22350cc6e3baf340f2bcdaa4e0c0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-24 10:14:31 +0530

    code and logic of issubset.py

--- DATA_STRUCTURES/DSA Questions/issubset.py
+++ DATA_STRUCTURES/DSA Questions/issubset.py
@@ -1,41 +1,28 @@
-"""
-Subset Sum Problem | DP-25
-Difficulty Level : Medium
- Last Updated : 09 Nov, 2020
-
-Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
-
-Example:
-
-Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
-Output: True
-There is a subset (4, 5) with sum 9.
-Logic =We are using the D.P Top down approach for the problem.(It is 0/1 knapsack problem
-
-"""
-def sumsubset(arr,cap,n):
-    dp = ([[False for i in range(sum + 1)]
-           for i in range(n + 1)])
-    for i in range(n+1):
-        for j in range(cap+1):
+def subset(arr,w):
+    l=len(arr)
+    dp=[[0 for i in range(w+1)]for j in range(l+1)]
+    for i in range(l+1):
+        for j in range(w+1):
             if i==0:
-                dp[i][j]=False
+                dp[i][j]=0
             if j==0:
-                dp[i][j]=True
-    for i in range(n+1):
-        for j in range(cap+1):
+                dp[i][j]=1
+    for i in range(1,l+1):
+        for j in range(1,w+1):
             if arr[i-1]<=j:
-                dp[i][j]=dp[i][j-arr[i-1]] or dp[i-1][j]
+                dp[i][j]=(dp[i-1][j-arr[i-1]] or dp[i-1][j])
             else:
-                dp[i][j] =dp[i-1][j]
-    return dp[n][cap]
+                dp[i][j]=dp[i-1][j]
+
+
+    print(dp)
+    return dp[l][w]
+
 if __name__ == '__main__':
     arr=[3, 34, 4, 12, 5, 2]
-    sum=9
-    n=len(arr)+1
-    net=sum+1
-    y=sumsubset(arr,sum,len(arr))
-    if y==True:
+    w=30
+    m=subset(arr,w)
+    if m==1:
         print("Yes found")
     else:
-        print("No")
+        print('Not found baby')
