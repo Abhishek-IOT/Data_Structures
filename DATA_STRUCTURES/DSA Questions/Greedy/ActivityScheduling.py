7dd580715b6a9490038a742a658964dd873c90e6
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-02-15 22:06:57 +0530

    ActivityScheduling.py code and logic

--- DATA_STRUCTURES/DSA Questions/Greedy/ActivityScheduling.py
+++ DATA_STRUCTURES/DSA Questions/Greedy/ActivityScheduling.py
@@ -1,21 +1,25 @@
 class Meeting:
-    def __init__(self, start, end, pos):
-        self.start = start
-        self.end = end
-        self.pos = pos
-
-
-def maxMeeting( l, n):
-    ans = []
-    l.sort(key=lambda x: x.end)
-    print(l)
-
-
+    def __init__(self,start,end,pos):
+        self.start=start
+        self.end=end
+        self.pos=pos
+def maxMeeting(l):
+    n=len(l)
+    l.sort(key=lambda x:x.end)
+    ans=[]
+    ans.append(l[0].pos)
+    time=l[0].end
+    for i in range(1,len(l)):
+        if l[i].start>time:
+            ans.append(l[i].pos)
+            time=l[i].end
+    for i in ans:
+        print(i+1,end="  ")
 if __name__ == '__main__':
-    s = [1, 3, 0, 5, 8, 5]
-    f = [2, 4, 6, 7, 9, 9]
-    n = len(s)
-    l = []
-    for i in range(n):
-        l.append(Meeting(s[i], f[i], i))
-    maxMeeting(l, n)
+
+    start=[ 1, 3, 0, 5, 8, 5 ]
+    finish=[ 2, 4, 6, 7, 9, 9 ]
+    l=[]
+    for i in range(len(start)):
+       l.append(Meeting(start[i],finish[i],i))
+    maxMeeting(l)
