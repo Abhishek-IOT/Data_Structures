99344baf8ef08c6bdb291a5e1dddea5584bfee03
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-06 16:28:26 +0530

    Done with correction of wrong thing done before.

--- DATA_STRUCTURES/DSA Questions/Dynamic/01knapsack/0/1KnapsackUsingMem.py
+++ DATA_STRUCTURES/DSA Questions/Dynamic/01knapsack/0/1KnapsackUsingMem.py
@@ -1,22 +1,19 @@
-def knapsack(w,val,wt,n,dp):
-    if n==0 or w==0:
+def knapsack(w,val,wt,n):
+    dp=[[-1 for i in range(wt+1) ]for j in range(n+1)]
+    if n==0 or wt==0:
         return 0
     if dp[n][wt]!=-1:
         return dp[n][wt]
     if w[n-1]<=wt:
-        dp[n][wt]=max(val[n-1]+
-                     knapsack(w,val,wt-w[n-1],n-1,dp),
-                     knapsack(w,val,wt,n-1,dp))
+        dp[n][wt]=max(val[n-1]+knapsack(w,val,wt-w[n-1],n-1),knapsack(w,val,wt,n-1))
     else:
-        dp[n][wt]=knapsack(w,val,wt,n-1,dp)
+        dp[n][wt]=knapsack(w,val,wt,n-1)
     return dp[n][wt]
+
 if __name__ == '__main__':
+    w=[1,3,5,6]
     n=4
     wt=5
-    dp = [[-1 for i in range(wt + 1)] for j in range(n + 1)]
-    w=[1,3,5,6]
     val=[1,4,2,3]
-    m=knapsack(w,val,wt,n,dp)
+    m=knapsack(w,val,wt,n)
     print("Maximum Profit=",m)
-
-
