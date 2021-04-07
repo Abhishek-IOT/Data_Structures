e8ebae1ed15517c69dabc257dab4c39f55a8a4c6
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-07 16:28:16 +0530

    Done the practice of Subset Problem.

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,22 +1,33 @@
-def knapsack(w,val,wt,n,dp):
-    if n==0 or w==0:
-        return 0
-    if dp[n][wt]!=-1:
-        return dp[n][wt]
-    if w[n-1]<=wt:
-        dp[n][wt]=max(val[n-1]+
-                     knapsack(w,val,wt-w[n-1],n-1,dp),
-                     knapsack(w,val,wt,n-1,dp))
-    else:
-        dp[n][wt]=knapsack(w,val,wt,n-1,dp)
-    return dp[n][wt]
-if __name__ == '__main__':
-    n=4
-    wt=5
-    dp = [[-1 for i in range(wt + 1)] for j in range(n + 1)]
-    w=[1,3,5,6]
-    val=[1,4,2,3]
-    m=knapsack(w,val,wt,n,dp)
-    print("Maximum Profit=",m)
+def Subsetsum(arr,n,s):
+    dp=[[False for i in range(s+1)]for i in range(n+1)]
+    for i in range(n+1):
+        for j in range(s+1):
+            if i==0:
+                dp[i][j]=False
+            if j==0:
+                dp[i][j]=True
+    for i in range(1,n+1):
+        for j in range(1,s+1):
+            if arr[i-1]<=j:
+                dp[i][j]=dp[i-1][j-arr[i-1]] or dp[i-1][j]
+            else:
+                dp[i][j]=dp[i-1][j]
+    pirt(dp,n,s)
+    print(dp[0][1])
+    return dp[n][s]
+def pirt(Mat,row,col):
+    for i in range(row+1):
+        print("[",end=" ")
+        for j in range(col+1):
+            print(Mat[i][j],end=" ")
+        print("]")
 
+if __name__ == '__main__':
+    arr=[1,2,3]
+    s=5
+    m=Subsetsum(arr,len(arr),s)
+    if m==True:
+        print("Yes Found")
+    else:
+        print("NO ")
 
