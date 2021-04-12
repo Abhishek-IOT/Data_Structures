8746807e5a2696716da9de2e217eb62ab09815d7
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-12 19:13:12 +0530

    Correction made

--- DATA_STRUCTURES/Heap/HeapSort.py
+++ DATA_STRUCTURES/Heap/HeapSort.py
@@ -17,7 +17,7 @@ def heapsort(arr):
     for i in range(start,-1,-1):
         heapify(arr,n,i)
     for i in range(n-1,0,-1):
-        arr[i],arr[0]=arr[0],arr[i],
+        arr[i],arr[0]=arr[0],arr[i]
         heapify(arr,i,0)
 arr = [12, 11, 13, 5, 6, 7]
 heapsort(arr)
