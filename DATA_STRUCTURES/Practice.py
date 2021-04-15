99c0adae50be01498573b358b196f0b0d8948cc0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-15 18:29:51 +0530

    Praticed the question of First negative integer in subarray of length k.

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,33 +1,20 @@
-def Subsetsum(arr,n,s):
-    dp=[[False for i in range(s+1)]for i in range(n+1)]
-    for i in range(n+1):
-        for j in range(s+1):
-            if i==0:
-                dp[i][j]=False
-            if j==0:
-                dp[i][j]=True
-    for i in range(1,n+1):
-        for j in range(1,s+1):
-            if arr[i-1]<=j:
-                dp[i][j]=dp[i-1][j-arr[i-1]] or dp[i-1][j]
-            else:
-                dp[i][j]=dp[i-1][j]
-    pirt(dp,n,s)
-    print(dp[0][1])
-    return dp[n][s]
-def pirt(Mat,row,col):
-    for i in range(row+1):
-        print("[",end=" ")
-        for j in range(col+1):
-            print(Mat[i][j],end=" ")
-        print("]")
-
+def FirstNegativeNo(arr,n,k):
+    i=0
+    j=k-1
+    l=[]
+    while(j-i+1==k and j!=len(arr)):
+        flag=0
+        for o in range(i,j+1):
+            if arr[o]<0:
+                l.append(arr[o])
+                flag=1
+                break
+        if flag==0:
+            l.append(0)
+        i+=1
+        j=j+1
+    print(l)
 if __name__ == '__main__':
-    arr=[1,2,3]
-    s=5
-    m=Subsetsum(arr,len(arr),s)
-    if m==True:
-        print("Yes Found")
-    else:
-        print("NO ")
-
+    arr=[-8, 2, 3, -6, 10]
+    n=len(arr)
+    FirstNegativeNo(arr,n,k=2)
