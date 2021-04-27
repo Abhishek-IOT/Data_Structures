15d5eb54680790947b23bcd6877fe4e2039b2291
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-27 12:56:50 +0530

    Program for job sequening in Practice.py

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,55 +1,19 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-class LinkedList:
-    def __init__(self):
-        self.head=None
-    def addNode(self,data):
-        newNode=Node(data)
-        if self.head is None:
-            self.head=newNode
-            return
-        last=self.head
-        while(last.next):
-            last=last.next
-        last.next=newNode
-    def printList(self):
-        temp=self.head
-        while(temp):
-            print(temp.data,end="=>")
-            temp=temp.next
-    def toarray(self,arr):
-        temp=self.head
-        while(temp):
-            arr.append(temp.data)
-            temp=temp.next
-        return arr
-
-if __name__ == '__main__':
-    l=LinkedList()
-    l.addNode(1)
-    l.addNode(2)
-    l.addNode(3)
-    l.addNode(10)
-    l.printList()
-    print()
-    m=LinkedList()
-    m.addNode(18)
-    m.addNode(17)
-    m.addNode(19)
-    m.printList()
-    li=[]
-    first=m.toarray(li)
-    l.toarray(li)
-    print()
-    print(first)
-    first.sort()
-    print(first)
-    newList=LinkedList()
-    for i in range(len(first)):
-        newList.addNode(first[i])
-    newList.printList()
+arr = [['a', 2, 100],  # Job Array
+       ['b', 1, 19],
+       ['c', 2, 27],
+       ['d', 1, 25],
+       ['e', 3, 15]]
+arr.sort(key=lambda x:x[2])
+arr.reverse()
+print(*arr)
+def getmaxjobs(arr):
+      return arr[0][1]
+l=[0]*getmaxjobs(arr)
+print(l)
+for j in range(len(l)):
+       l[j]=arr[j][2]
+print(l)
+print(sum(l))
 
 
 
