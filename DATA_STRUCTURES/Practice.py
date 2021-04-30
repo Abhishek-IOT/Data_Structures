58e38e7a65d00723ec8203db3dd16234cc4eb5e2
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-04-30 20:07:08 +0530

    Code for codechef april lunch challenge.

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,19 +1,8 @@
-arr = [['a', 2, 100],  # Job Array
-       ['b', 1, 19],
-       ['c', 2, 27],
-       ['d', 1, 25],
-       ['e', 3, 15]]
-arr.sort(key=lambda x:x[2])
-arr.reverse()
-print(*arr)
-def getmaxjobs(arr):
-      return arr[0][1]
-l=[0]*getmaxjobs(arr)
-print(l)
-for j in range(len(l)):
-       l[j]=arr[j][2]
-print(l)
-print(sum(l))
-
-
-
+try:
+    test=int(input())
+    for i in range(test):
+        m,n=map(int,input().split(' '))
+        c=m//n
+        print(c)
+except:
+    pass
