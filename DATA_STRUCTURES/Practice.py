f8c66a84ec033bd2dcd34077a574193adf307238
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-05-06 12:08:00 +0530

    Just to Practice the Kadane ALgorithm .

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,40 +1,16 @@
-from collections import deque
-def shortest(start,target,dict):
-    if start==target:
-        return 0
-    if target not in dict:
-        return 0
-    level=0
-    wordlen=len(start)
-    qu=deque()
-    qu.append(start)
-    while(len(qu)>0):
-        level+=1
-        size=len(qu)
-        for i in range(size):
-            word=[j for j in qu.popleft()]
-            for pos in range(wordlen):
-                org=word[pos]
-                for c in range(ord('A'),ord('Z')+1):
-                    word[pos]=chr(c)
-                    print(word)
-                    if ("".join(word)==target):
-                        print(word,"56565656")
-                        return level+1
+def kadaneAlgo(arr):
+    max_end_here=0
+    max_so_far=0
+    for i in range(len(arr)):
+        max_end_here=max_end_here+arr[i]
+        if max_end_here>max_so_far:
+            max_so_far=max_end_here
+        if max_end_here<0:
+            max_end_here=0
 
-                    if ("".join(word) not in D):
-                        continue
-                    del D["".join(word)]
-                    qu.append("".join(word))
-                word[pos] = org
-    return 0
-if __name__ == '__main__':
-    D = {}
-    D['ABCD']=1
-    D['EBAD']=1
-    D['EBCD']=1
-    D['XYZA']=1
-    start="ABCV"
-    target="EBCD"
-    lev=shortest(start,target,D)
-    print(lev)
+    print(max_end_here)
+    return max_so_far
+if __name__=='__main__':
+    arr=[-2,-3,4,-1,-2,1,5,-3]
+    m=kadaneAlgo(arr)
+    print(m)
