af93a288f655010a2487e8c955a011610e27ee2f
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-05-07 19:01:28 +0530

    Program for codechef contest May long challenge.[Solubility Problem Code: SOLBLTY]

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,16 +1,9 @@
-def kadaneAlgo(arr):
-    max_end_here=0
-    max_so_far=0
-    for i in range(len(arr)):
-        max_end_here=max_end_here+arr[i]
-        if max_end_here>max_so_far:
-            max_so_far=max_end_here
-        if max_end_here<0:
-            max_end_here=0
+try:
+    test=int(input())
+    for i in range(test):
+        l,m,h=list(map(int,input().split(' ')))
+        ans=m+(100-l)*h
+        print(ans*10)
 
-    print(max_end_here)
-    return max_so_far
-if __name__=='__main__':
-    arr=[-2,-3,4,-1,-2,1,5,-3]
-    m=kadaneAlgo(arr)
-    print(m)
+except:
+    pass
