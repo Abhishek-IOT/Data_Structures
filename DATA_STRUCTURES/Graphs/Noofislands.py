8797b79b089c964a783e6d663dd84e4cbbda7be0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-05-10 12:37:56 +0530

    Added things in no of islands problem.

--- DATA_STRUCTURES/Graphs/Noofislands.py
+++ DATA_STRUCTURES/Graphs/Noofislands.py
@@ -1,48 +1,34 @@
-"""
-In this we have to go 8 directions that means diagonally also.
-"""
+from collections import defaultdict
 class Graph:
-    def __init__(self,row,col,g):
-        self.row=row
-        self.col=col
-        self.graph=g
-    def dfs(self,u,v,visited):
-        if u<0 or v<0 or v>=self.row or u>=self.col:
-            return
-        if self.graph[u][v]==0:
-            return
-        if visited[u][v]==False:
-            visited[u][v]=1
-            self.dfs(u+1,v,visited)
-            self.dfs(u-1,v,visited)
-            self.dfs(u,v+1,visited)
-            self.dfs(u,v-1,visited)
-            self.dfs(u+1,v+1,visited)
-            self.dfs(u-1,v-1,visited)
-            self.dfs(u+1,v-1,visited)
-            self.dfs(u-1,v+1,visited)
-
-
-    def countislands(self):
-
-        visited=[[False for i in range(self.col)]for j in range(self.row)]
-        c=0
-        for i in range(self.row):
-            for j in range(self.col):
-                visited[i][j]=0
-        for i in range(self.row):
-            for j in range(self.col):
-                if visited[i][j]==False and self.graph[i][j]==1:
-                    self.dfs(i,j,visited)
-                    c=c+1
-        return c
+    def __init__(self,vertices):
+        self.vertices=vertices
+        self.graph=defaultdict(list)
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+    def findsubset(self,parent,i):
+        if parent[i]==-1:
+            return i
+        if parent[i]!=-1:
+            return self.findsubset(parent,parent[i])
+    def union(self,parent,x,y):
+        parent[x]=y
+    def iscyclic(self):
+        parent=[-1]*(self.vertices)
+        for i in self.graph:
+            for j in self.graph[i]:
+                x=self.findsubset(parent,i)
+                y=self.findsubset(parent,j)
+                if x==y:
+                    return True
+                self.union(parent,x,y)
 if __name__ == '__main__':
-    graph = [[1, 1, 0, 0, 0],
-             [0, 1, 0, 0, 1],
-             [1, 0, 0, 1, 1],
-             [0, 0, 0, 0, 0],
-             [1, 0, 1, 0, 1]]
-    row=len(graph)
-    col=len(graph[0])
-    g=Graph(row,col,graph)
-    print(g.countislands())
+    g=Graph(3)
+    g.addEdge(0, 1)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    if g.iscyclic():
+        print("YEs")
+    else:
+        print("No")
+
+
