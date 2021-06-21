83552719c3882d1f0c5ea86c42d57361adb6a810
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-06-21 14:32:49 +0530

    Done the problems but was not able to commit .(Now Commiting)

--- DATA_STRUCTURES/Practice.py
+++ DATA_STRUCTURES/Practice.py
@@ -1,19 +1,52 @@
-def delmid(s):
-    if len(s)==0:
-        return -1
-    k=(len(s)//2)+1
-    print(k)
-    solve(s,k)
-    return s
-def solve(s,k):
-    if k==1:
-        s.pop()
-        return
-    temp=s[-1]
-    s.pop()
-    solve(s,k-1)
-    s.append(temp)
-    return
-s=[1,2,3,4,5,6]
-m=delmid(s)
-print(m)
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def addatbeg(self,data):
+        node=Node(data)
+        node.next=self.head
+        self.head=node
+    def printList(self):
+        node=self.head
+        while(node!=None):
+            print(node.data,end='=>')
+            node=node.next
+    def palindrome(self,head):
+        slow=self.head
+        fast=self.head
+        stack=[]
+        while(fast!=None and fast.next!=None):
+            stack.append(slow.data)
+            slow=slow.next
+            fast=fast.next.next
+        if fast!=None:
+            slow=slow.next
+        print(slow.data)
+        print(stack)
+        while(slow.next!=None):
+            if slow.data!=stack[-1]:
+                return False
+            else:
+                slow=slow.next
+        return True
+
+if __name__ == '__main__':
+    l=LinkedList()
+    l.addatbeg(1)
+    l.addatbeg(0)
+    l.addatbeg(2)
+    l.addatbeg(0)
+    l.addatbeg(1)
+    l.printList()
+    print()
+    if l.palindrome(l.head):
+        print("Yes")
+    else:
+        print("No")
+
+
+
+
